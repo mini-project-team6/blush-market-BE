83c94ecc5cfef3aa2356025e95abdb462328df44
Parkjaeyong <pjjy92@gmail.com>
2023-02-24 19:42:34 +0900

    feat : 로그인 기능 구현  및 패스워드 암호화#4
    - 회원가입 시 구현하지 못한 패스워드 암호화 적용
    - 회원 로그인 시 암호화 된 패스워드와 비교
    - 토큰 정상 발급
    
    이슈 :
    - 토큰이 만료되는 시점에 어떻게 연결을 지속시킬 것인가 ?

--- src/main/java/com/sparta/blushmarket/entity/Member.java
+++ src/main/java/com/sparta/blushmarket/entity/Member.java
@@ -1,25 +1,30 @@
 package com.sparta.blushmarket.entity;
 
+import com.sparta.blushmarket.dto.SignupRequestDto;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Entity
 @NoArgsConstructor
+
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private String password;
 
-
-
+    @Builder
+    public Member(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
 }
---
+++ src/main/java/com/sparta/blushmarket/service/MemberService.java
@@ -0,0 +1,63 @@
+package com.sparta.blushmarket.service;
+
+import com.sparta.blushmarket.dto.LoginRequestDto;
+import com.sparta.blushmarket.dto.StatusMsgResponseDto;
+import com.sparta.blushmarket.entity.Member;
+import com.sparta.blushmarket.jwt.JwtUtil;
+import com.sparta.blushmarket.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final JwtUtil jwtUtil;
+
+    private final PasswordEncoder passwordEncoder;
+
+//    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    public ResponseEntity<StatusMsgResponseDto> signup(String userName, String password){
+        // 회원가입 유저가 있는지 확인하는 부분
+        // 기존에 id체크하는 부분이 있어서 이 부분 관련 협의 필요
+        memberCheck(userName);
+        memberRepository.save(new Member(userName,passwordEncoder.encode(password)));
+        return ResponseEntity.ok(new StatusMsgResponseDto("회원가입 성공", HttpStatus.OK.value()));
+    }
+
+    /**
+     * 로그인 기능
+     */
+    public ResponseEntity<StatusMsgResponseDto> login(String userName, String password, HttpServletRequest request) {
+
+        Optional<Member> findMemeber = memberRepository.findByName(userName);
+        if(findMemeber.isEmpty() || !passwordEncoder.matches(password,findMemeber.get().getPassword())){
+            throw new IllegalStateException("회원이 존재하지 않거나 패스워드가 일치하지 않습니다");
+        }
+
+        return ResponseEntity.ok()
+                .header(JwtUtil.AUTHORIZATION_HEADER,jwtUtil.createToken(findMemeber.get().getName()))
+                .body(new StatusMsgResponseDto("로그인 완료", HttpStatus.OK.value()));
+    }
+
+    /**
+     * 등록된 멤버인지 확인하는 기능
+     */
+    public void memberCheck(String username) {
+        Optional<Member> findMember = memberRepository.findByName(username);
+        if(findMember.isPresent()){
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+        }
+    }
+
+
+}

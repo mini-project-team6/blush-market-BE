c8c8ce83d02b9c2cbe7e33ee18751bc8d5727944
JaeHun <clickyour@gmail.com>
2023-02-28 10:04:04 +0900

    refactor : #50 return HTTPStatus변경

--- src/main/java/com/sparta/blushmarket/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/blushmarket/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ package com.sparta.blushmarket.exception;
 import com.sparta.blushmarket.common.ApiResponseDto;
 import com.sparta.blushmarket.common.ErrorResponse;
 import com.sparta.blushmarket.common.ResponseUtils;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
-    protected ApiResponseDto<ErrorResponse> customException(CustomException e){
+    protected ResponseEntity<ApiResponseDto<ErrorResponse>> customException(CustomException e){
 
 //        return ErrorResponse.toResponseEntity(e.getExceptionEnum());
-        return ResponseUtils.error(ErrorResponse.of(e.getExceptionEnum().getCode(),e.getExceptionEnum().getMsg()));
+        return ResponseEntity.badRequest().body(ResponseUtils.error(ErrorResponse.of(e.getExceptionEnum().getCode(),e.getExceptionEnum().getMsg())));
 
     }
 

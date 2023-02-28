ed001ae229c20f8e9248246d5b21d411958d3a8f
Parkjaeyong <pjjy92@gmail.com>
2023-02-28 15:41:22 +0900

    feat : 회원 가입 시 유효성 검사 - close #26
    valid 항목
    - 패스워드 : 영문자,숫자,특수문자 포함 8자 이상
    - 이메일 : 이메일 형식에 맞는지 체크

--- src/main/java/com/sparta/blushmarket/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/blushmarket/dto/SignupRequestDto.java
@@ -2,9 +2,16 @@ package com.sparta.blushmarket.dto;
 
 import lombok.Getter;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
+
 @Getter
 public class SignupRequestDto {
     private String name;
+
+    @Pattern(regexp = "^[a-zA-Z0-9\\\\d`~!@#$%^&*()-_=+]{8,}$", message = "패스워드에러")
     private String password;
+
+    @Pattern(regexp = "[0-9a-zA-Z]+(.[_a-z0-9-]+)*@(?:\\\\w+\\\\.)+\\\\w+$",message = "이메일형식불일치")
     private String email;
 }
